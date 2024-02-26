66f14cdcdba69e09d9396e25e6b2a94e6214bfa6
victor-val <vval26v@gmail.com>
2024-02-26 12:05:30 +0100

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import { CarteleraComponent } from './cartelera/cartelera.component';
+import { DetalleEventoComponent } from './detalle-evento/detalle-evento.component';
 
 
 const routes: Routes = [
   { path: '', component: CarteleraComponent },
   { path: 'cartelera', component: CarteleraComponent },
+  { path: 'detalle-evento', component: DetalleEventoComponent },
 ];
 
 @NgModule({
---
+++ src/app/detalle-evento/detalle-evento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-evento',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalle-evento.component.html',
+  styleUrl: './detalle-evento.component.scss'
+})
+export class DetalleEventoComponent {
+
+}
